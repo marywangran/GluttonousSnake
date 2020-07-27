994b4f4f7d5897d50bc2a8c71baeda160fff811f
marywangran <marywangran@126.com>
2020-07-27 19:31:45 +0800

    Update snake.c

--- snake.c
+++ snake.c
@@ -1,15 +1,17 @@
-#include <string.h>
 #include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
 
-int sx = 10, sy = 10;
 #define	LINES	30
-#define	COLS	30
+#define	COLS	60
+
+int start_x = 10, start_y = 10;
 int main(int argc,char* argv[])
 {
-	int i, j, c, hx, hy, tx, ty, fx, fy;
+	int i, j, c, head_x, head_y, tail_x, tail_y, fx, fy;
 	char dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	int head_dir_index = 3, tail_dir_index = 3;
 	char pad[LINES][COLS];
-	int hidx = 3, tidx = 3;
 
 	initscr();
 	noecho();
@@ -18,9 +20,19 @@ int main(int argc,char* argv[])
 	curs_set(0);
 
 	memset(pad, 0, sizeof(pad));
-	pad[sy][sx] = 1;
-	hx = tx = sx;
-	hy = ty = sy;
+	pad[start_y][start_x] = 1;
+	head_x = tail_x = start_x;
+	head_y = tail_y = start_y;
+	srand(time(NULL));
+
+	for (i = 0; i < LINES; i ++) {
+		pad[i][0] = 1;
+		pad[i][COLS - 1] = 1;
+	}
+	for (i = 0; i < COLS; i ++) {
+		pad[0][i] = 1;
+		pad[LINES - 1][i] = 1;
+	}
 
 	while (1) {
 		fx = rand()%COLS;
@@ -31,48 +43,47 @@ int main(int argc,char* argv[])
 
 		while (c = getch()) {
 			if (c == KEY_UP)
-				hidx = 0;
+				head_dir_index = 0;
 			else if (c == KEY_DOWN)
-				hidx = 1;
+				head_dir_index = 1;
 			else if (c == KEY_LEFT)
-				hidx = 2;
+				head_dir_index = 2;
 			else if (c == KEY_RIGHT)
-				hidx = 3;
-			hy += dir[hidx][0];
-			hx += dir[hidx][1];
-			if (pad[hy][hx] == 1 || hy > LINES || hx > COLS || hx < 0 || hy < 0) {
-				clear();
-				mvprintw(COLS/2, LINES/2, "game over");
+				head_dir_index = 3;
+			head_y += dir[head_dir_index][0];
+			head_x += dir[head_dir_index][1];
+			if (head_y >= LINES - 1 || head_x >= COLS - 1 || head_x <= 0 || head_y <= 0 ||
+				pad[head_y][head_x] == 1) {
+				mvprintw(LINES/2, (COLS - 9)/2, "game over");
 				refresh();
-				sleep(100);
+				sleep(2);
+				endwin();
+				exit(1);
 			}
-			if (pad[hy][hx] == 2) {
-				pad[hy][hx] = 1;
+			if (pad[head_y][head_x] == 2) {
+				pad[head_y][head_x] = 1;
 				break;
 			}
 
-			pad[hy][hx] = 1;
-			pad[ty][tx] = 0;
-			if (pad[ty - 1][tx] == 1)
-				tidx = 0;
-			else if (pad[ty + 1][tx] == 1)
-				tidx = 1;
-			else if (pad[ty][tx - 1] == 1)
-				tidx = 2;
-			else if (pad[ty][tx + 1] == 1)
-				tidx = 3;
-			ty += dir[tidx][0];
-			tx += dir[tidx][1];
-			for (i = 0; i < LINES; i ++) {
-				for (j = 0; j < COLS; j ++) {
+			pad[head_y][head_x] = 1;
+			pad[tail_y][tail_x] = 0;
+			if (tail_y - 1 > 0 && pad[tail_y - 1][tail_x] == 1)
+				tail_dir_index = 0;
+			else if (tail_y + 1 < LINES - 1 && pad[tail_y + 1][tail_x] == 1)
+				tail_dir_index = 1;
+			else if (tail_x - 1 > 0 && pad[tail_y][tail_x - 1] == 1)
+				tail_dir_index = 2;
+			else if (tail_x + 1 < COLS -1 && pad[tail_y][tail_x + 1] == 1)
+				tail_dir_index = 3;
+			tail_y += dir[tail_dir_index][0];
+			tail_x += dir[tail_dir_index][1];
+			for (i = 0; i < LINES; i ++)
+				for (j = 0; j < COLS; j ++)
 					mvprintw(i, j, pad[i][j]?"#":" ");
-				}
-			}
 			refresh();
-			usleep(500000);
+			usleep(200000);
 		}
 	}
 
-	endwin();
 	return 0;
 }
